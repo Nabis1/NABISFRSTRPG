c8783afb4ec4688c89494b90627a8ada24262e1a
Nabis <r.cernil332@gmail.com>
2024-01-21 20:33:42 +0100

    Added button, make a Home section in grid, picture not fixed yet

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -9,15 +9,23 @@ namespace Engine.ViewModels
     public class GameSession
     {
         public Player CurrentPlayer { get; set; }
+        public Location CurrentLocation { get; set; }
         public GameSession()
         {
             CurrentPlayer = new Player();
             CurrentPlayer.Name = "Jaydris";
-            CurrentPlayer.CharacterClass = "Fighter";
-            CurrentPlayer.HitPoints = 10;
+            CurrentPlayer.CharacterClass = "Warrior";
+            CurrentPlayer.HitPoints = 100;
             CurrentPlayer.Gold = 10;
             CurrentPlayer.ExperiencePoints = 0;
             CurrentPlayer.Level = 1;
+
+            CurrentLocation = new Location();
+            CurrentLocation.Name = "Home";
+            CurrentLocation.XCoordinate = 0;
+            CurrentLocation.YCoordinate = -1;
+            CurrentLocation.Description = "This is your Home";
+            CurrentLocation.ImageName = "/Engine;component/Images/Locations/Home.png";
         }
     }
 }
--- JustRPG/MainWindow.xaml.cs
+++ JustRPG/MainWindow.xaml.cs
@@ -26,5 +26,9 @@ namespace WPFUI
             _gameSession = new GameSession();
             DataContext = _gameSession;
         }
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            _gameSession.CurrentPlayer.ExperiencePoints = _gameSession.CurrentPlayer.ExperiencePoints + 10;
+        }
     }
 }
