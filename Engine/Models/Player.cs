25dde06364ab0a7a5aa4659c94c8e9414e1d9ea7
Nabis <r.cernil332@gmail.com>
2024-03-03 01:18:05 +0100

    Refactoring Base Class for Player, Monster, and Trader

--- Engine/Models/Player.cs
+++ Engine/Models/Player.cs
@@ -8,24 +8,13 @@ using System.Xml.Linq;
 using System.Collections.ObjectModel;
 namespace Engine.Models
 {
-    public class Player : BaseNotificationClass
-    {
+    public class Player : LivingEntity
+    {  
         #region Properties
-        private string _name;
         private string _characterClass;
-        private int _hitPoints;
         private int _experiencePoints;
         private int _level;
-        private int _gold;
-        public string Name
-        {
-            get { return _name; }
-            set
-            {
-                _name = value;
-                OnPropertyChanged(nameof(Name));
-            }
-        }
+       
         public string CharacterClass
         {
             get { return _characterClass; }
@@ -33,15 +22,6 @@ namespace Engine.Models
             {
                 _characterClass = value;
                 OnPropertyChanged(nameof(CharacterClass));
-            }
-        }
-        public int HitPoints
-        {
-            get { return _hitPoints; }
-            set
-            {
-                _hitPoints = value;
-                OnPropertyChanged(nameof(HitPoints));
             }
         }
         public int ExperiencePoints
@@ -62,34 +42,11 @@ namespace Engine.Models
                 OnPropertyChanged(nameof(Level));
             }
         }
-        public int Gold
-        {
-            get { return _gold; }
-            set
-            {
-                _gold = value;
-                OnPropertyChanged(nameof(Gold));
-            }
-        }
-        public ObservableCollection<GameItem> Inventory { get; set; }
-        public List<GameItem> Weapons =>
-            Inventory.Where(i => i is Weapon).ToList();
         public ObservableCollection<QuestStatus> Quests { get; set; }
         #endregion
         public Player()
         {
-            Inventory = new ObservableCollection<GameItem>();
             Quests = new ObservableCollection<QuestStatus>();
-        }
-        public void AddItemToInventory(GameItem item)
-        {
-            Inventory.Add(item);
-            OnPropertyChanged(nameof(Weapons));
-        }
-        public void RemoveItemFromInventory(GameItem item)
-        {
-            Inventory.Remove(item);
-            OnPropertyChanged(nameof(Weapons));
         }
         public bool HasAllTheseItems(List<ItemQuantity> items)
         {
--- Engine/Models/Trader.cs
+++ Engine/Models/Trader.cs
@@ -7,23 +7,12 @@ using System.Collections.ObjectModel;
 
 namespace Engine.Models
 {
-    public class Trader : BaseNotificationClass
+    public class Trader : LivingEntity
     {
-        public string Name { get; set; }
-        public ObservableCollection<GameItem> Inventory { get; set; }
         public Trader(string name) 
         {
             Name = name;
-            Inventory = new ObservableCollection<GameItem>();
         }
-        public void AddItemToInventory (GameItem item)
-        { 
-            Inventory.Add(item);
-        }
-        public void RemoveItemFromInventory (GameItem item)
-        {
-            Inventory.Remove(item);
-        }
-
+       
     }
 }
