3f0db4235a3029f926eaaf7d6ea1959bce998495
Nabis <r.cernil332@gmail.com>
2024-03-18 22:49:09 +0100

    Catch and Log Exceptions

---
+++ Engine/Services/LoggingService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Engine.Services
+{
+    public static class LoggingService
+    {
+        private const string LOG_FILE_DIRECTORY = "Logs";
+        static LoggingService()
+        {
+            string logDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, LOG_FILE_DIRECTORY);
+            if (!Directory.Exists(logDirectory))
+            {
+                Directory.CreateDirectory(logDirectory);
+            }
+        }
+        public static void Log(Exception exception, bool isInnerException = false)
+        {
+            using (StreamWriter sw = new StreamWriter(LogFileName(), true))
+            {
+                sw.WriteLine(isInnerException ? "INNER EXCEPTION" : $"EXCEPTION:{DateTime.Now}");
+                sw.WriteLine(new string(isInnerException ? '-' : '=', 40));
+                sw.WriteLine($"{exception.Message}");
+                sw.WriteLine($"{exception.StackTrace}");
+                sw.WriteLine();
+            }
+            if (exception.InnerException != null)
+            {
+                Log(exception.InnerException, true);
+            }
+        }
+        private static string LogFileName()
+        {
+            return Path.Combine(AppDomain.CurrentDomain.BaseDirectory, LOG_FILE_DIRECTORY, $"NABISFRSTRPG{DateTime.Now:yyyyMMDD.log}");
+        }
+    }
+
+}
--- JustRPG/App.xaml.cs
+++ JustRPG/App.xaml.cs
@@ -1,6 +1,6 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
+using System.Windows.Threading;
+using Engine.Services;
 
 namespace JustRPG
 {
@@ -9,6 +9,14 @@ namespace JustRPG
     /// </summary>
     public partial class App : Application
     {
+        private void App_OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            string exceptionMessageText =
+                $"An exception occurred: {e.Exception.Message}\r\n\r\nat: {e.Exception.StackTrace}";
+            LoggingService.Log(e.Exception);
+            // TODO: Create a Window to display the exception information.
+            MessageBox.Show(exceptionMessageText, "Unhandled Exception", MessageBoxButton.OK);
+        }
     }
 
 }
