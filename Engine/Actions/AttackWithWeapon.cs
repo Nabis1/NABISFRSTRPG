6f63bf965fdb4564e26e39d2cf8700bad804e6b9
Nabis <r.cernil332@gmail.com>
2024-04-06 23:31:17 +0200

    Remove CombatService dependency from model classes

--- Engine/Actions/AttackWithWeapon.cs
+++ Engine/Actions/AttackWithWeapon.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Engine.Models;
 using Engine.Services;
+using Engine.Shared;
 using NABISFRSTRPG.Core;
 namespace Engine.Actions
 {
@@ -24,7 +25,7 @@ namespace Engine.Actions
         {
             string actorName = (actor is Player) ? "You" : $"The {actor.Name.ToLower()}";
             string targetName = (target is Player) ? "you" : $"the {target.Name.ToLower()}";
-            if (CombatService.AttackSucceeded(actor, target))
+            if (AttackSucceeded(actor, target))
             {
                 int damage = DiceService.Instance.Roll(_damageDice).Value;
                 ReportResult($"{actorName} hit {targetName} for {damage} point{(damage > 1 ? "s" : "")}.");
@@ -34,6 +35,18 @@ namespace Engine.Actions
             {
                 ReportResult($"{actorName} missed {targetName}.");
             }
+        }
+        private static bool AttackSucceeded(LivingEntity attacker, LivingEntity target)
+        {
+            // Currently using the same formula as FirstAttacker initiative.
+            // This will change as we include attack/defense skills,
+            // armor, weapon bonuses, enchantments/curses, etc.
+            int playerDexterity = attacker.GetAttribute("DEX").ModifiedValue * attacker.GetAttribute("DEX").ModifiedValue;
+            int opponentDexterity = target.GetAttribute("DEX").ModifiedValue * target.GetAttribute("DEX").ModifiedValue;
+            decimal dexterityOffset = (playerDexterity - opponentDexterity) / 10m;
+            int randomOffset = DiceService.Instance.Roll(20).Value - 10;
+            decimal totalOffset = dexterityOffset + randomOffset;
+            return DiceService.Instance.Roll(100).Value <= 50 + totalOffset;
         }
     }
 }
